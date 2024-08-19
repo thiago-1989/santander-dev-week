b039e0dc041294daf51f45626e341d8b5d6d4cef
Thiago Oliveira <thiagosoaresccb@gmail.com>
2024-08-19 20:34:44 -0300

    Implementação da camada de dominio

---
+++ src/main/java/edu/thiago/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package edu.thiago.domain.repository;
+
+
+import edu.thiago.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
