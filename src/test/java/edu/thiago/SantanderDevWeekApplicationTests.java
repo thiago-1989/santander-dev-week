96bd745444df76726942a2d4afe88f0a9b9aa37d
Thiago Oliveira <thiagosoaresccb@gmail.com>
2024-08-19 19:04:10 -0300

    Initial commit

---
+++ src/test/java/edu/thiago/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
